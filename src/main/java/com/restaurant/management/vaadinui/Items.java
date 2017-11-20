4729f14aff25a89e7433e37ab6f32653979abf07
Coding-Squad <islammasum34@gmail.com>
2017-11-21 02:04:20 +0600

    few changes

--- src/main/java/com/restaurant/management/vaadinui/Items.java
+++ src/main/java/com/restaurant/management/vaadinui/Items.java
@@ -1,8 +1,23 @@
 package com.restaurant.management.vaadinui;
 
 import com.vaadin.ui.VerticalLayout;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
 @Component
+@Entity
+@Table(name="items")
 public class Items  extends VerticalLayout{
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private double price;
+
+
 }
--- src/main/java/com/restaurant/management/vaadinui/VaadinUI.java
+++ src/main/java/com/restaurant/management/vaadinui/VaadinUI.java
@@ -1,9 +1,11 @@
 package com.restaurant.management.vaadinui;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -31,30 +33,42 @@ public class VaadinUI extends UI{
 
     private void setUplayout() {
         verticalLayout = new VerticalLayout();
+        verticalLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
         setContent(verticalLayout);
     }
 
     private void addHeader() {
         Label header = new Label("Menu Items");
+        addStyleName(ValoTheme.LABEL_H1);
+        header.setSizeUndefined();
         verticalLayout.addComponent(header);
     }
 
     private void addForm() {
         HorizontalLayout formLayout = new HorizontalLayout();
+        formLayout.setSpacing(true);
+        formLayout.setWidth("80%");
+        Label itemNameLabel = new Label("Name:");
         TextField itemName = new TextField();
+        Label itemPriceLabel = new Label("Price:");
         TextField itemPrice = new TextField();
         Button addButton = new Button("Add Items");
-        formLayout.addComponents(itemName, itemPrice, addButton);
+        addButton.setIcon(FontAwesome.PLUS);
+        addButton.setStyleName(ValoTheme.BUTTON_PRIMARY);
+        formLayout.addComponents(itemNameLabel, itemName, itemPriceLabel, itemPrice, addButton);
         verticalLayout.addComponent(formLayout);
     }
 
     private void addItemList() {
+        itemLists.setWidth("80%");
         verticalLayout.addComponent(itemLists);
     }
 
     private void addActionButton() {
-        Button deleteButton = new Button("Delete Completed");
+        Button deleteButton = new Button("Delete");
 
         verticalLayout.addComponent(deleteButton);
+        deleteButton.setIcon(FontAwesome.MINUS);
+        deleteButton.setStyleName(ValoTheme.BUTTON_DANGER);
     }
 }
