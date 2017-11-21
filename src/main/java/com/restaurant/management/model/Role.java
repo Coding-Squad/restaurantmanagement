1d8f5e3ae21fff045e2f8b435391a90cd305cd7b
Masum-Islam <islamasum@gmail.com>
2017-11-21 11:03:52 +0600

    junks cleared

--- src/main/java/com/restaurant/management/model/Role.java
+++ src/main/java/com/restaurant/management/model/Role.java
@@ -1,7 +1,12 @@
 package com.restaurant.management.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Programmer on 21-Nov-17.
  */
+@Entity
+@Table(name = "role")
 public class Role {
 }
--- src/main/java/com/restaurant/management/model/Users.java
+++ src/main/java/com/restaurant/management/model/Users.java
@@ -1,7 +1,13 @@
 package com.restaurant.management.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Programmer on 21-Nov-17.
  */
+
+@Entity
+@Table(name = "users")
 public class Users {
 }
